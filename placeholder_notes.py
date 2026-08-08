53d71dda93163c9e997e58297d32c67c5bd9c1f8
Mihir Tandon <85353517+micro-7@users.noreply.github.com>
2026-08-08 11:10:41 +0530

    Add files via upload

---
+++ placeholder_notes.py
@@ -0,0 +1,3 @@
+TODO: implement the loan disbursement logic here later this week
+Need to talk to the compliance team first before writing this
+Placeholder file, nothing here yet, will fill in after the meeting
